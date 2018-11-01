23de78fe49907c63255196b7db4c140903c6739b
Jacob Baldwin <jacob.ryan.baldwin@gmail.com>
2018-11-01 11:51:42 -0600

    upvote and downvote functions

--- backend/datastore.js
+++ backend/datastore.js
@@ -1,9 +1,19 @@
 datastore = {
+  next_person_id: 10,
+
   persons: [
     {
-      name: "jacob"
+      id: 1,
+      name: "Jacob",
+      bio: "Just Jacob",
+      image_filename: "jacob.jpg",
+      gender: "m",
+      age: 22,
+      upvotes: 0,
+      downvotes: 0
     }
   ]
+
 }
 
 module.exports = datastore;
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,8 +3,31 @@ var router = express.Router();
 
 var datastore = require('../datastore');
 
-/* GET users listing. */
 router.get('/persons', function(req, res, next) {
+  res.json(datastore);
+});
+
+// format is /api/upvote?id={pid}
+router.get('/upvote', (req, res, next) => {
+  let persons = datastore.persons;
+  let id = req.query.id;
+
+  let i = persons.findIndex(x => x.id == id);
+
+  persons[i].upvotes++;
+
+  res.json(datastore);
+});
+
+// format is /api/downvote?id={pid}
+router.get('/downvote', (req, res, next) => {
+  let persons = datastore.persons;
+  let id = req.query.id;
+
+  let i = persons.findIndex(x => x.id == id);
+
+  persons[i].upvotes--;
+
   res.json(datastore);
 });
 
