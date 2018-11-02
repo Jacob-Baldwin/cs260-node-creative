d8058fd2e19ab64e3c62096da0081607ccf5821d
Jacob Baldwin <jacob.ryan.baldwin@gmail.com>
2018-11-01 22:30:20 -0600

    remptied datastore

--- backend/datastore.js
+++ backend/datastore.js
@@ -1,27 +1,8 @@
 datastore = {
-  next_person_id: 3,
+  next_person_id: 1,
 
   persons: [
-    {
-      id: 1,
-      name: "Jacob",
-      bio: "Just Jacob",
-      image_filename: "jacob.jpg",
-      gender: "m",
-      age: 22,
-      upvotes: 0,
-      downvotes: 0
-    },
-    {
-      id: 2,
-      name: "alex",
-      bio: "Just alex",
-      image_filename: "alex.png",
-      gender: "f",
-      age: 19,
-      upvotes: 0,
-      downvotes: 0
-    }
+
   ]
 
 }
