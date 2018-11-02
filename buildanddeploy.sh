38ce1f4a6f164132e83ebfb0f13926eb49718b3e
Jacob Baldwin <jacob.ryan.baldwin@gmail.com>
2018-11-01 20:31:35 -0600

    Update buildanddeploy.sh

--- buildanddeploy.sh
+++ buildanddeploy.sh
@@ -1,3 +1,3 @@
 docker build -t localhost:5000/cs260_node_creative .
-docker push localhost:5000/node_creative
+docker push localhost:5000/cs260_node_creative
 docker service update --image localhost:5000/cs260_node_creative cs260_node_creative
